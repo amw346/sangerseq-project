6a4ef8c6154d563818abb0ed762fa8958f0fa380
amw346 <amw346@cornell.edu>
2017-09-19 16:34:01 -0400

    Add files via upload
    Added new function that uses makebasecalls to accomplish task 3

--- getPriSeq.R
+++ getPriSeq.R
@@ -118,10 +118,9 @@ fourpeaks <- function(sangob, ratio) {
 	Tpeaks = sangob@traceMatrix[,4]
 	len = dim(sangob@traceMatrix)[1]
 	
-	
 	maxpeak = matrix(nrow = len, ncol = 1)
-	for (i in len) {
-		maxpeak[i] = max(sang@traceMatrix[1,])
+	for (i in 1:len) {
+		maxpeak[i] =max(sangob@traceMatrix[i,])
 	}
 	
 	Aratio = Apeaks/maxpeak
@@ -129,25 +128,54 @@ fourpeaks <- function(sangob, ratio) {
 	Gratio = Tpeaks/maxpeak
 	Tratio = Tpeaks/maxpeak
 	
-	Acol = matrix(nrow= len, ncol = 1)
+	Acol = matrix(0L,nrow= len, ncol = 1)
+	Ccol = matrix(0L, nrow= len, ncol = 1)
+	Gcol = matrix(0L, nrow= len, ncol = 1)
+	Tcol = matrix(0L, nrow= len, ncol = 1)
+	
+	primary = matrix(nrow= len)
+	secondary= matrix(nrow= len) 
+	warningposition = matrix()
 	for (i in len) {
-	 if (Aratio[i] > ratio) {
-	 	Acol[i] = "A"
-	 }
-	 if (Cratio[i] > ratio) {
-	 	Ccol[i] = "C"
-	 }
-	 if (Gratio[i] > ratio) {
-	 	Gcol[i] = "G"
-	 }
-	 if (Tratio[i] > ratio) {
-	 	Tcol[i] = "T"
+	 if (maxpeak[i] == 0) {
+	 	primary[i] = "N"
+	 	secondary [i] = "N"
 	 }
 	 
+	 warning = 0
+	 if (Aratio[i] > ratio) {
+	 	Acol[i] = Aratio[i]
+	 	warning = warning +1
+	 }
+	 
+	 if (Cratio[i] > ratio) {
+	 	Ccol[i] = Cratio[i]
+	 	warning = warning +1
+	 }
+	 
+	 if (Gratio[i] > ratio) {
+	 	Gcol[i] = Gratio[i]
+	 	warning = warning +1
+	 }
+	 
+	 if (Tratio[i] > ratio) {
+	 	Tcol[i] = Tratio[i]
+	 	warning = warning +1
+	 }
+	 if (warning == 3) | (warning == 4) {
+	  warningposition = cbind(warningposition,i)
+	 }
+	 ACGT = cbind(Aratio[i],Cratio[i],Gratio[i],Tratio[i])
+	 max = maxcol(Aratio[i],Cratio[i],Gratio[i],Tratio[i])
+	 if max == 1 {
+	 	
+	 }
+	 
+	  
 	}
-	SeqString = addDNACol(Aratio, Cratio, Gratio, Tratio)
-	return( SeqString)
-	
+	print("More than two bases above threshold check following positions:")
+	print(warningposition)
+	return() 	
 }
 
 addDNACol <- function(Aratio,Cratio,Gratio,Tratio) {
@@ -159,3 +187,66 @@ addDNACol <- function(Aratio,Cratio,Gratio,Tratio) {
 addBase <- function(nuc1,nuc2) {
 	if 
 }
+
+
+test1 <- function(sangob, len) {
+	maxpeak = matrix(nrow = len, ncol = 1)
+	for (i in 1:10) {
+		maxpeak[i] =max(sangob@traceMatrix[1,])
+		print ( "There was error at ")
+		print(maxpeak[i])
+	}
+	return(maxpeak[1:100])
+}
+
+
+cutpoint <- function(sangob) {
+	pri = primarySeq(sangob,string = TRUE)
+	len = nchar(pri)
+	num = 0
+	found = FALSE
+	for (i in 1:len) {
+		c1= substr(pri,i,i)
+		c2 = substr(pri,i+1,i+1)
+		c3 = substr(pri,i+2,i+2)
+		if (found) {
+			return(num-2)
+			}
+	 	if ((c1 == "N") & c2 == "N" & c3 == "N") {
+	 		found = TRUE
+	 	}
+	 	num = 1 + i
+	 	}
+	 return(num-2) 
+}
+
+
+test7 <- function(sangob, ratio, start) {
+	pri = primarySeq(sangob,string = TRUE)
+	len = nchar(pri)
+	cutpoint = cutpoint(sangob)
+	hetcalls = makeBaseCalls(sangob, ratio)
+	chromatogram(hetcalls, width = 75, height = 4, showcalls = "both", 
+	              trim5 = start, trim3 =  len - cutpoint)
+	newpri = substr(primarySeq(hetcalls, string = TRUE), start, cutpoint)
+	sec = secondarySeq(hetcalls, string =TRUE)
+    len = nchar(newpri)
+    warningpos = matrix()
+    for (i in 1:len) {
+    	b = substr(sec,i, i)
+    	if ( (b != "A") & (b != "C") & (b != "G") & (b != "T")) {
+    		warningpos = cbind(warningpos,i)
+    	}
+    }
+    if (dim(warningpos)[2] > 1 ) {
+    	print("Warning: More than two peaks above threshold at following positions:")
+    	return(warningpos)	
+    }
+    return ("No warnings")
+}
+
+
+
+
+
+
