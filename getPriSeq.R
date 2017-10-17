8308580ca3dab8b706f411dddcdd00637898390c
amw346 <amw346@cornell.edu>
2017-10-17 16:32:37 -0400

    Add files via upload

--- getPriSeq.R
+++ getPriSeq.R
@@ -84,169 +84,362 @@ addPriSec <- function(pri,sec) {
 	 return (AddedSeq = combined)
 }
 
-
-clipToString <- function(sangob) {
-	pri = primarySeq(sangob,string = TRUE)
-	len = nchar(pri)
-	num = 0
-	found = FALSE
-	for (i in 1:len) {
-		c1= substr(pri,i,i)
-		c2 = substr(pri,i+1,i+1)
-		c3 = substr(pri,i+2,i+2)
-		if (found) {
-			return(substr(pri,20,num-2))
-			}
-	 	if ((c1 == "N") & c2 == "N" & c3 == "N") {
-	 		found = TRUE
-	 	}
-	 	num = i +1
-	 	}
-	 return (sang@primarySeq)
+library(seqinr)
+file2 = read.fasta(file="Z:/Shared Documents/Alicia Williams/r/test1.fas", as.string = TRUE)
+view(file)
+n = c(1:118)
+seq= data.frame(n)
+filenames <- list.files("Z:/Shared Documents/Alicia Williams/haplotypes", pattern= "*.fas", full.name = TRUE)
+namelist = data.frame(n)
+name <- list.files("Z:/Shared Documents/Alicia Williams/haplotypes", pattern= "*.fas")
+for (i in 1:118) {
+  namelist[i,1] = filenames[i]
+  seq[i,1] = read.fasta(file=filenames[i], as.string = FALSE, forceDNAtolower = TRUE, set.attributes = FALSE)
 }
 
-newPrimary <- function(sangob, string) {
-# A function to input a sanger object and a string and reassign the primary sequence of a sangerobject to that string as a DNAString. Example code to put in command line: sang <- newPrimary(sang,sangstring) where sang is a sanger object and sang string is the desired new primary string.
-	sangob@primarySeq<- DNAString(string)
-	return(sangob)
-}
+seq1 = seq[3,1]
+seq2 = seq[2,1]
 
-fourpeaks <- function(sangob, ratio) {
-	Apeaks = sangob@traceMatrix[,1]
-	Cpeaks = sangob@traceMatrix[,2]
-	Gpeaks = sangob@traceMatrix[,3]
-	Tpeaks = sangob@traceMatrix[,4]
-	len = dim(sangob@traceMatrix)[1]
-	
-	maxpeak = matrix(nrow = len, ncol = 1)
-	for (i in 1:len) {
-		maxpeak[i] =max(sangob@traceMatrix[i,])
-	}
-	
-	Aratio = Apeaks/maxpeak
-	Cratio = Cpeaks/maxpeak
-	Gratio = Tpeaks/maxpeak
-	Tratio = Tpeaks/maxpeak
-	
-	Acol = matrix(0L,nrow= len, ncol = 1)
-	Ccol = matrix(0L, nrow= len, ncol = 1)
-	Gcol = matrix(0L, nrow= len, ncol = 1)
-	Tcol = matrix(0L, nrow= len, ncol = 1)
-	
-	primary = matrix(nrow= len)
-	secondary= matrix(nrow= len) 
-	warningposition = matrix()
-	for (i in len) {
-	 if (maxpeak[i] == 0) {
-	 	primary[i] = "N"
-	 	secondary [i] = "N"
-	 }
-	 
-	 warning = 0
-	 if (Aratio[i] > ratio) {
-	 	Acol[i] = Aratio[i]
-	 	warning = warning +1
-	 }
-	 
-	 if (Cratio[i] > ratio) {
-	 	Ccol[i] = Cratio[i]
-	 	warning = warning +1
-	 }
-	 
-	 if (Gratio[i] > ratio) {
-	 	Gcol[i] = Gratio[i]
-	 	warning = warning +1
-	 }
-	 
-	 if (Tratio[i] > ratio) {
-	 	Tcol[i] = Tratio[i]
-	 	warning = warning +1
-	 }
-	 if (warning == 3) | (warning == 4) {
-	  warningposition = cbind(warningposition,i)
-	 }
-	 ACGT = cbind(Aratio[i],Cratio[i],Gratio[i],Tratio[i])
-	 max = maxcol(Aratio[i],Cratio[i],Gratio[i],Tratio[i])
-	 if max == 1 {
-	 	
-	 }
-	 
-	  
-	}
-	print("More than two bases above threshold check following positions:")
-	print(warningposition)
-	return() 	
-}
+allign = pairwiseAllignment(seq1, seq2)
 
-addDNACol <- function(Aratio,Cratio,Gratio,Tratio) {
-	len = dim(Aratio)[1]
-	for i in 
+library(sangerseqR)
+read.fasta("Z:/Shared Documents/Alicia Williams/haplotypes/kdr-his6 sans exons.fas")
+readsangerseq("/Users/amw346/Desktop/test2.ab1")
+source("https://bioconductor.org/biocLite.R")
+biocLite("Biostrings")
+
+install.packages("sangerseqR", lib= "C:/Users/amw346/Documents/R/win-library/3.4")
+
+
+library(seqinr)
+file2 = read.fasta(file="Z:/Shared Documents/Alicia Williams/r/test1.fas", as.string = TRUE)
+view(file)
+
+library(seqinr)
+n = c(1:118)
+seq= data.frame(n)
+filenames <- list.files("Z:/Shared Documents/Alicia Williams/haplotypes", pattern= "*.fas", full.name = TRUE)
+namelist = data.frame(n)
+name <- list.files("Z:/Shared Documents/Alicia Williams/haplotypes", pattern= "*.fas")
+for (i in 1:118) {
+  namelist[i,1] = filenames[i]
+  seq[i,1] = read.fasta(file=filenames[i], as.string = TRUE, forceDNAtolower = FALSE, set.attributes = FALSE)
+}
+library(sangerseqR)
+read.fasta("Z:/Shared Documents/Alicia Williams/haplotypes/kdr-his6 sans exons.fas")
+
+seq2 = "cgacttcatgcacagcttcatgattgtgttccgagtgctgtgcggagagtggatcgagtccatgtgggactgcatgtatgtgggcgatgtcagctgtatacccttcttcttggccacggtcgtgataggcaatcttgtggtaagttgacgtggccgaaactactcccccgctcccaggatggaggcttctgatggccaattaaaaaagaacatttattttaaatcaacctctctctttctctctctctctcaactttattccgtccatcctttgcaggttcttaat"
+
+seq1 = "gaatttcaccgacttcatgcacagcttcatgattgtgttccgagtgctgtgcggagagtggatcgagtccatgtgggactgcatgtatgttggcgatgtgagctgtatacccttcttcttggccacggtcgtgatcggcaatcatgtggtaagttgacgtggccgaaactactcccccgctcccaggagagaggcttgatccgtaatatacaaaaatttgacatttatctctctctttctctctcccaactttattctctccactgttgcaggttcttaat"
+seq4 = "gaatttcaccgacttcatgcacagcttcatgattgtgttccgagtgctgtgcggagagtggatcgagtccatgtgggactgcatgtatgttggcgatgtgagctgtatacccttcttcttggccacggtcgtgatcggcaatcatgtggtaagttgacgtggccgaaactactcccccgctcccaggagagaggcttgatccgtaatatacaaaaatttgacatttatctctaactttattctctccactgttgcaggttcttaat"
+nchar(seq4)
+nchar(seq3)
+seq5 = "gaatttcaccgacttcatgcacagcttcatgattgtgttccgagtgctgtgcggagagtggatcgagtccatgtgggactgcatgtatgttggcgatgtgagctgtatacccttcttcttggccacggtcgtgatcggcaatcatgtggtaagttgacgtggccgaaactactcccccgctcccaggagagaggcttgatccgtaatatacaaaaatttgacatttatctctaactttatt"
+
+seq3 = "ccgagtgctgtgcggagagtggatcgagtccatgtgggactgcatgtatgttggcgatgtgagctgtatacccttcttcttggccacggtcgtgatcggcaatcatgtggtaagttgacgtggccgaaactactcccccgctcccaggagagaggcttgatccgtaatatacaaaaatttgacatttatctctctctttctctctcccaactttattctctccactgttgcaggttcttaagggggt"
+allign = pairwiseAlignment(seq1,seq2)
+writePairwiseAlignments(allign)
+
+combinedtypes = matrix[]
+#TODo nest loops: for i 1:117, y = 118 -x , for i in 1:y
+n = 118
+for (i in 1:n) {
+  x = FALSE
+  seq1 = seq[i,1]
+  seq2 = seq[i+1,1]
+  allign = pairwiseAlignment(seq1,seq2)
+  writePairwiseAlignments(allign)
+  allign@start
+  
+  allign@ranges
+  str(allign)
+  allign@pattern@unaligned@ranges
+  x= mismatchTable(allign)
+  bstring = aligned(allign)
+  str(bstring)
+  bstring@ranges
+  des@start 
+  start = allign@start
+  start = compareStrings(allign)
+  indel(allign)
+  deletion(allign)
+  nedit(allign)
+  DNAString(seq1)
+  DNAString(seq2)
+  summary(allign)
+  summary(x)
+  
+  
+  if (startseq1>1){
+    x  = TRUE
+  }
+  
+  combined =addseq(seq1,seq2, start, x)
+  combinedtypes.append(combined)
 }
 
 
-addBase <- function(nuc1,nuc2) {
-	if 
+addseq <- function(seq1,seq2, start,x) {
+  short = seq2
+  long = seq1
+  if (x) {
+    short = seq2
+    long = seq1
+  }
+  len = nchar(long)
+  #cut long string
+  newlong = substr(long,start,len)
+  len2 = nchar(sec)
+  
+  #adding the elements
+  #TODO: diff lengths issue
+  combined = ""
+  for (i in 1:len) {
+    newchar = addbases(substr(short,i,i), substr(long,i,i))
+    combined= paste0(combined, newchar)
+  }
+  
+  #returning the added sequence
+  return (combined)
 }
 
 
-test1 <- function(sangob, len) {
-	maxpeak = matrix(nrow = len, ncol = 1)
-	for (i in 1:10) {
-		maxpeak[i] =max(sangob@traceMatrix[1,])
-		print ( "There was error at ")
-		print(maxpeak[i])
-	}
-	return(maxpeak[1:100])
+addbases <- function(a,b) {
+  #addbases inputs two single character strings and outputs a single letter string according to IUPAC codes
+  if (a == "A") {
+    if (b == "A") {return ("A")}
+    if (b == "C") {return ("M")}
+    if (b == "G") {return ("R")}
+    if (b == "T") {return ("W")}
+    if (b == "N") {return ("N")}
+    if (b == "R") {return ("R")}
+    if (b == "Y") {return ("H")}
+    if (b == "S") {return ("V")}
+    if (b == "W") {return ("W")}
+    if (b == "K") {return ("D")}
+    if (b == "M") {return ("M")}
+  }
+  if (a == "C") {
+    if (b == "A") {return ("M")}
+    if (b == "C") {return ("C")}
+    if (b == "G") {return ("S")}
+    if (b == "T") {return ("Y")}
+    if (b == "N") {return ("N")}
+    if (b == "R") {return ("V")}
+    if (b == "Y") {return ("Y")}
+    if (b == "S") {return ("S")}
+    if (b == "W") {return ("H")}
+    if (b == "K") {return ("B")}
+    if (b == "M") {return ("M")}
+  }
+  if (a == "G") {
+    if (b == "A") {return ("R")}
+    if (b == "C") {return ("S")}
+    if (b == "G") {return ("G")}
+    if (b == "T") {return ("K")}
+    if (b == "N") {return ("N")}
+    if (b == "R") {return ("R")}
+    if (b == "Y") {return ("B")}
+    if (b == "S") {return ("S")}
+    if (b == "W") {return ("D")}
+    if (b == "K") {return ("K")}
+    if (b == "M") {return ("V")}
+  }
+  if (a == "T") {
+    if (b == "A") {return ("W")}
+    if (b == "C") {return ("Y")}
+    if (b == "G") {return ("K")}
+    if (b == "T") {return ("T")}
+    if (b == "N") {return ("N")}
+    if (b == "R") {return ("D")}
+    if (b == "Y") {return ("Y")}
+    if (b == "S") {return ("B")}
+    if (b == "W") {return ("W")}
+    if (b == "K") {return ("K")}
+    if (b == "M") {return ("H")}
+  }
+  if (a == "N") {return("N")}
+  return ("input strings not valid")	
 }
 
 
-cutpoint <- function(sangob) {
-	pri = primarySeq(sangob,string = TRUE)
-	len = nchar(pri)
-	num = 0
-	found = FALSE
-	for (i in 1:len) {
-		c1= substr(pri,i,i)
-		c2 = substr(pri,i+1,i+1)
-		c3 = substr(pri,i+2,i+2)
-		if (found) {
-			return(num-2)
-			}
-	 	if ((c1 == "N") & c2 == "N" & c3 == "N") {
-	 		found = TRUE
-	 	}
-	 	num = 1 + i
-	 	}
-	 return(num-2) 
-}
+combinedtypes = matrix[] 
+ 
 
 
-test7 <- function(sangob, ratio, start) {
-	pri = primarySeq(sangob,string = TRUE)
-	len = nchar(pri)
-	cutpoint = cutpoint(sangob)
-	hetcalls = makeBaseCalls(sangob, ratio)
-	chromatogram(hetcalls, width = 75, height = 4, showcalls = "both", 
-	              trim5 = start, trim3 =  len - cutpoint)
-	newpri = substr(primarySeq(hetcalls, string = TRUE), start, cutpoint)
-	sec = secondarySeq(hetcalls, string =TRUE)
-    len = nchar(newpri)
-    warningpos = matrix()
-    for (i in 1:len) {
-    	b = substr(sec,i, i)
-    	if ( (b != "A") & (b != "C") & (b != "G") & (b != "T")) {
-    		warningpos = cbind(warningpos,i)
-    	}
-    }
-    if (dim(warningpos)[2] > 1 ) {
-    	print("Warning: More than two peaks above threshold at following positions:")
-    	return(warningpos)	
-    }
-    return ("No warnings")
-}
+ for (d in 1:117) { 
+   y = 118-d 
+    
+   for (i in 1:y) { 
+        
+         seq1 = seq[d,1] 
+         seq2 = seq[i+d,1] 
+          
+         allign = pairwiseAlignment(seq1,seq4) 
+         allign = pairwiseAlignment(seq3,seq5)
+         allign2 = pairwiseAlignment(seq5,seq3)
+         
+         writePairwiseAlignments(allign2)
+         s = summary(allign) 
+          allign2@pattern@range 
+         nchar(seq5)
+         nchar(seq3)
+        
+        x = "NaN" 
+        index = 0 
+        while (x == "NaN") { 
+         index = 1 +index 
+             x = s@mismatchSummary$pattern$position[index,3] 
+        } 
+        # seq1len =nchar(seq1)
+         #cutSeq1 =FALSE
+         #if (seq1len = index -1 + allign@pattern@range@width){ 
+        #     cutSeq1 = FALSE
+        # } 
+         if  (allign@pattern@range@start == 1) {
+           cutSeq1 == FALSE
+         }
+         combined =addseq(seq1,seq2, index, CutSeq1) 
+         DNAString(combined)
+         combinedtypes[i] = combined 
+       } 
+   } 
+ 
+str(allign)
+ 
 
+ addseq <- function(seq1,seq2, index, CutSeq1) { 
+     short = seq1 
+     long = seq2 
+     if (CutSeq1) { 
+         short = seq2 
+         long = seq1 
+       } 
+     len = nchar(long) 
+     #cut long string 
+     newlong = substr(long,start,len) 
+      
+     lenlong = nchar(newlong) 
+     lenshort = nchar(short) 
+    length = lenlong 
+    if (lenlong > lenshort) { 
+         length = lenshort 
+      } 
+      
+     #adding the elements 
+     #TODO: diff lengths issue 
+    combined = "" 
+     for (i in 1:length) { 
+         newchar = addbases(substr(short,i,i), substr(long,i,i)) 
+         combined= paste0(combined, newchar) 
+       } 
+      
+     #returning the added sequence 
+     return (combined) 
+   } 
 
+ #10-12-17
+ library(seqinr)
+ n = c(1:118)
+ seq= data.frame(n)
+ filenames <- list.files("Z:/Shared Documents/Alicia Williams/haplotypes", pattern= "*.fas", full.name = TRUE)
+ namelist = data.frame(n)
+ name <- list.files("Z:/Shared Documents/Alicia Williams/haplotypes", pattern= "*.fas")
+ for (i in 1:118) {
+   namelist[i,1] = filenames[i]
+   seq[i,1] = read.fasta(file=filenames[i], as.string = TRUE, forceDNAtolower = TRUE, set.attributes = FALSE)
+ }
+ 
+ combinedtypes = matrix() 
+ for (d in 1:117) { 
+   y = 118-d 
+   
+   for (i in 1:y) { 
+     
+     seq1 = seq[d,1] 
+     seq2 = seq[i+d,1] 
+     
+     allign = pairwiseAlignment(seq1,seq2)
+     
+     s = summary(allign) 
 
+     #define start index
+     x = "NaN" 
+     index = 0 
+     while (x == "NaN") { 
+       index = 1 +index 
+       x = s@mismatchSummary$pattern$position[index,3] 
+     } 
+    
+     cutSeq1 = TRUE
+     #define cutseq1 boolean
+     if  (allign@pattern@range@start == 1) {
+       cutSeq1 == FALSE
+     }
+     
+     combined =addseq(seq1,seq2, index, cutSeq1) 
+     DNAString(combined)
+     combinedtypes = rbind(combinedtypes,combined)
+   } 
+ }
 
-
-
+ addseq <- function(seq1,seq2, index, CutSeq1) { 
+   short = seq1 
+   long = seq2 
+   if (CutSeq1) { 
+     short = seq2 
+     long = seq1 
+   } 
+   len = nchar(long) 
+   #cut long string 
+   newlong = substr(long,index,len) 
+   
+   lenlong = nchar(newlong) 
+   lenshort = nchar(short) 
+   length = lenlong 
+   if (lenlong > lenshort) { 
+     length = lenshort 
+   } 
+   
+   #adding the elements 
+   combined = "" 
+   for (i in 1:length) { 
+     newchar = addbases(substr(short,i,i), substr(long,i,i)) 
+     combined= paste0(combined, newchar) 
+   } 
+   
+   #returning the added sequence 
+   return (combined) 
+ }
+ 
+ addbases <- function(a,b) {
+   #addbases inputs two single character strings and outputs a single letter string according to IUPAC codes
+   if (a == "a") {
+     if (b == "a") {return ("A")}
+     if (b == "c") {return ("M")}
+     if (b == "g") {return ("R")}
+     if (b == "t") {return ("W")}
+   }
+   
+   if (a == "c") {
+     if (b == "a") {return ("M")}
+     if (b == "c") {return ("C")}
+     if (b == "g") {return ("S")}
+     if (b == "t") {return ("Y")}
+   }
+   
+   if (a == "g") {
+     if (b == "a") {return ("R")}
+     if (b == "c") {return ("S")}
+     if (b == "g") {return ("G")}
+     if (b == "t") {return ("K")}
+   }
+   if (a == "t") {
+     if (b == "a") {return ("W")}
+     if (b == "c") {return ("Y")}
+     if (b == "g") {return ("K")}
+     if (b == "t") {return ("T")}
+   }
+   if (a == "N") {return("N")}
+   return ("input strings not valid")	
+ }
