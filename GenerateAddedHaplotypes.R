3e70aa9c71771a9ce8a3a1b759aa0b3a0aa39da4
amw346 <amw346@cornell.edu>
2017-10-20 15:11:12 -0400

    Add files via upload
    edits on the combineHap function and new addbases2, and new addPriSec2 and checkmasterlist function is added for the first time

--- GenerateAddedHaplotypes.R
+++ GenerateAddedHaplotypes.R
@@ -267,7 +267,7 @@ clipToString <- function(sangob) {
 } 
 
 
-#modified
+#modified for compareHAP
 clipIndex <- function(sangob) { 
   pri = primarySeq(sangob,string = TRUE) 
   len = nchar(pri) 
@@ -288,16 +288,112 @@ clipIndex <- function(sangob) {
   return (num-2)
 } 
 
+#10202017 more edits
 compareHap<- function(file) {
   sangerobj <- readsangerseq(file)
+  index = clipIndex(sangerobj)
   basecalls <- makeBaseCalls(sangerobj)
   pri <- primarySeq(basecalls, string = 'TRUE')
   sec <- secondarySeq(basecalls, string = 'TRUE')
-  index = clipIndex(sangerobj)
   cutpri = substr(pri,20,index)
   cutsec = substr(sec,20,index)
- p = pairwiseAlignment(cutpri,cutsec)
-  writePairwiseAlignments(p)
+  combined = addPriSec2(cutpri,cutsec)
+  checkMasterList(combined)
+}
+
+checkMasterList<- function(seq, master) {
+  len = dim(list)[1]
+  found = FALSE
+  for (i in 2:len) {
+    #compare
+    master[i]
+    if (found = TRUE) {
+      print("match found")
+      return(list[i])
+    }
+  }
+  print("no match found") 
+}
+  
+
+p = pairwiseAlignment(combined,seqA)
+writePairwiseAlignments(p)
+
+addPriSec2 <- function(pri,sec) {
+  #addPriSeq inputs two strings of same length and outputs a string that is the combined version according to IUPAC codes
+  
+  #checking if same length otherwise error message
+  len = nchar(cutpri)
+  len2 = nchar(cutsec)
+  if (len != len2) {return ("input sequences different lengths")}
+  
+  #adding the elements
+  combined = ""
+  for (i in 1:len) {
+    newchar = addbases2(substr(pri,i,i), substr(sec,i,i))
+    combined= paste0(combined, newchar)
+  }
+  
+  #returning the added sequence
+  return (AddedSeq = combined)
+}
+
+addbases2 <- function(a,b) {
+  #addbases inputs two single character strings and outputs a single letter string according to IUPAC codes
+  if (a == "A") {
+    if (b == "A") {return ("A")}
+    if (b == "C") {return ("M")}
+    if (b == "G") {return ("R")}
+    if (b == "T") {return ("W")}
+    if (b == "N") {return ("N")}
+    if (b == "R") {return ("R")}
+    if (b == "Y") {return ("H")}
+    if (b == "S") {return ("V")}
+    if (b == "W") {return ("W")}
+    if (b == "K") {return ("D")}
+    if (b == "M") {return ("M")}
+  }
+  if (a == "C") {
+    if (b == "A") {return ("M")}
+    if (b == "C") {return ("C")}
+    if (b == "G") {return ("S")}
+    if (b == "T") {return ("Y")}
+    if (b == "N") {return ("N")}
+    if (b == "R") {return ("V")}
+    if (b == "Y") {return ("Y")}
+    if (b == "S") {return ("S")}
+    if (b == "W") {return ("H")}
+    if (b == "K") {return ("B")}
+    if (b == "M") {return ("M")}
+  }
+  if (a == "G") {
+    if (b == "A") {return ("R")}
+    if (b == "C") {return ("S")}
+    if (b == "G") {return ("G")}
+    if (b == "T") {return ("K")}
+    if (b == "N") {return ("N")}
+    if (b == "R") {return ("R")}
+    if (b == "Y") {return ("B")}
+    if (b == "S") {return ("S")}
+    if (b == "W") {return ("D")}
+    if (b == "K") {return ("K")}
+    if (b == "M") {return ("V")}
+  }
+  if (a == "T") {
+    if (b == "A") {return ("W")}
+    if (b == "C") {return ("Y")}
+    if (b == "G") {return ("K")}
+    if (b == "T") {return ("T")}
+    if (b == "N") {return ("N")}
+    if (b == "R") {return ("D")}
+    if (b == "Y") {return ("Y")}
+    if (b == "S") {return ("B")}
+    if (b == "W") {return ("W")}
+    if (b == "K") {return ("K")}
+    if (b == "M") {return ("H")}
+  }
+  if (a == "N") {return("N")}
+  return ("input strings not valid")	
 }
 
 
