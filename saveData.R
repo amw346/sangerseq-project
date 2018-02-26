e0825bf3e6ad79742919680b782f4590d9054d26
amw346 <amw346@cornell.edu>
2018-02-26 10:22:36 -0500

    Add files via upload
    File to write most current data files to .csv files

---
+++ saveData.R
@@ -0,0 +1,9 @@
+#save important dataframes
+newCombinedNoGapAllAdded7021 = newCombinedNoGapAllAdded
+write.csv(newCombinedNoGapAllAdded7021, "C:/Users/amw346/Desktop/newCombinedNoGapAllAdded7021.csv")
+write.csv(seq, "C:/Users/amw346/Desktop/seq118.csv")
+
+#contains index of pair in NewCombinedNoGap pair 1 information, index of pair in newCOmbinedNOGap, pair 2 info and then last 4 columns corespond to which pair its from, last col is counting if match is due to 3 pairs that are the same
+write.csv(matchesCOMBONoGap2, "C:/Users/amw346/Desktop/matchesCOMBONoGap505.csv")
+
+write.csv(matchesog, "C:/Users/amw346/Desktop/matchesog3.csv")
