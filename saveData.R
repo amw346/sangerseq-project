41dd19eb2aa94039f6a1c54b67091a0fadad52e8
amw346 <amw346@cornell.edu>
2018-02-27 12:12:03 -0500

    Add files via upload
    saved more files and put some explanations for what they are

--- saveData.R
+++ saveData.R
@@ -7,3 +7,24 @@ write.csv(seq, "C:/Users/amw346/Desktop/seq118.csv")
 write.csv(matchesCOMBONoGap2, "C:/Users/amw346/Desktop/matchesCOMBONoGap505.csv")
 
 write.csv(matchesog, "C:/Users/amw346/Desktop/matchesog3.csv")
+
+#code for saving the evolution of newCombined
+write.csv(combinedtypes,"C:/Users/amw346/Desktop/evo/combinedtypes6903archive.csv")
+write.csv(newCombined, "C:/Users/amw346/Desktop/evo/newCombined6903archive.csv")
+write.csv(newCombinedNoGap, "C:/Users/amw346/Desktop/evo/newCombinedNoGap6903archive.csv")
+write.csv(newCombinedNoGapAllAdded, "C:/Users/amw346/Desktop/evo/newCombinedNoGapAllAdded7021archive.csv")
+
+write.csv(namelist,"C:/Users/amw346/Desktop/evo/namelist118.csv")
+
+#investigation of matches
+#dataframe that is all the sequences that contain v101 or v70
+#and will therefore have a match with at least one other
+#contains combined sequences, names, index in newCombinedNewGap
+write.csv(samepairs,"C:/Users/amw346/Desktop/evo/samepairs10170_233.csv")
+write.csv(samepairs1691,"C:/Users/amw346/Desktop/evo/samepairs1691_233.csv")
+
+#dataframe of indexes in NewCombinedNewGap of both pairs that are matches
+#original 587 matches 
+write.csv(matchesCOMBO,"C:/Users/amw346/Desktop/evo/matchesCOMBOog587_archive.csv")
+#reduced to 505 after dealing with gap / added beginning part
+write.csv(matchesCOMBONoGap,"C:/Users/amw346/Desktop/evo/matchesCOMBONoGap505_archive.csv")
